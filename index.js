6df0be4d946ac5920b18729bd9cc7d05a7700ca8
Nicolas Iglesias <nfiglesias@gmail.com>
2020-09-18 17:19:25 -0300

    fix return

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ var providers = {
 		mainnet: {
 			blockchain: function (addr) {
 				return request.get('https://blockchain.info/q/addressbalance/' + addr).send().then(function (res) {
-					return parseFloat(res.body);
+					return parseFloat(res.text || res.body);
 				});
 			}
 		},
 		testnet: {
 			blockchain: function (addr) {
 				return request.get('https://testnet.blockchain.info/q/addressbalance/' + addr).send().then(function (res) {
-					return parseFloat(res.body);
+					return parseFloat(res.text || res.body);
 				});
 			}
 		}
